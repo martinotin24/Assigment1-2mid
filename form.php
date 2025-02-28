a12feee4a168cec4229b6d2ea4a7b7330b7192c4
martinotin24 <72896552+martinotin24@users.noreply.github.com>
2025-02-27 22:58:06 -0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Input Form</title>
+</head>
+<body>
+    <h2>Enter Five Numbers</h2>
+    <form action="process.php" method="GET">
+        <label>Number A:</label> <input type="number" name="a" required><br>
+        <label>Number B:</label> <input type="number" name="b" required><br>
+        <label>Number C:</label> <input type="number" name="c" required><br>
+        <label>Number D:</label> <input type="number" name="d" required><br>
+        <label>Number E:</label> <input type="number" name="e" required><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
